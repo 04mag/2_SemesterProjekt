2692e10b34eb9f4c886ad8b5a8f9f1d76a26683e
04mag <mathiasgreisen@gmail.com>
2024-11-14 22:05:31 +0100

    Added MekanikerRenderComponent

---
+++ Anden_SemesterProjekt/Client/Components/Ansatte/Mekanikere/MekanikerRenderComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Anden_SemesterProjekt.Client.Components.Ansatte.Mekanikere
+{
+    public partial class MekanikerRenderComponent
+    {
+        [Parameter, EditorRequired]
+        public Mekaniker Mekaniker { get; set; }
+    }
+}
