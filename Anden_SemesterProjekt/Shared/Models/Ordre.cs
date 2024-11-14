ee0acdfac5a8e2410b3ea6c1dc1368d3179a3145
Sorenbb97 <sorenbb97@gmail.com>
2024-11-14 11:35:50 +0100

    Varelinje opdateret

--- Anden_SemesterProjekt/Shared/Models/Ordre.cs
+++ Anden_SemesterProjekt/Shared/Models/Ordre.cs
@@ -9,7 +9,7 @@ namespace Anden_SemesterProjekt.Shared.Models
     public class Ordre
     {
         public int Id { get; set; }
-        public List<OrdreLinje> OrdreLinjer { get; set; }
+        public List<VareLinje> OrdreLinjer { get; set; }
         public Kunde Kunde { get; set; }
         public DateTime Dato { get; set; }
         public Betaling Betaling { get; set; }
---
+++ Anden_SemesterProjekt/Shared/Models/VareLinje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anden_SemesterProjekt.Shared.Models
+{
+    public class VareLinje
+    {
+        public int VareLinjeId { get; set; }
+        public int OrdreId { get; set; }
+        public Ordre Ordre { get; set; }
+        public int VareId { get; set; }
+        public Vare Vare { get; set; }
+        public int Antal { get; set; }
+        public double? Rabat { get; set; }
+        public double VarePris { get; set; }
+    }
+}
+
+    
