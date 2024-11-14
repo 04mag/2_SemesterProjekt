386a8dedb22e5efdafc8c4909f4f00749df19dac
04mag <mathiasgreisen@gmail.com>
2024-11-14 19:56:38 +0100

    Added IAnsatClientService

---
+++ Anden_SemesterProjekt/Client/Services/IAnsatClientService.cs
@@ -0,0 +1,10 @@
+﻿using Anden_SemesterProjekt.Shared.Models;
+
+namespace Anden_SemesterProjekt.Client.Services
+{
+    public interface IAnsatClientService
+    {
+        public Task<List<Mekaniker>> GetMekanikere();
+        public Task<Mekaniker> GetMekaniker(int id);
+    }
+}
