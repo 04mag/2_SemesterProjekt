f6845141676c566a73fb3a85b6761076cfeb17a0
Sorenbb97 <sorenbb97@gmail.com>
2024-11-14 11:46:45 +0100

    Udlejning opdateret

--- Anden_SemesterProjekt/Shared/Models/Udlejning.cs
+++ Anden_SemesterProjekt/Shared/Models/Udlejning.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Anden_SemesterProjekt.Shared.Models
 {
-    public class Udlejning : Vare
+    public class Udlejning
     {
-        public UdlejningsScooter Scooter { get; set; }
+        public int UdlejningId { get; set; }
+        public int ScooterId { get; set; }
+        public UdlejningsScooter UdlejningsScooter { get; set; }
+        public int OrdreId { get; set; }
+        public Ordre Ordre { get; set; }
         public DateTime StartDato { get; set; }
         public DateTime SlutDato { get; set; }
-        public string Noter { get; set; }
+        public double AntalKmKørt { get; set; }
+        public bool SelvrisikoUdløst { get; set; }
+        public double lejePrDag { get; set; }
+        public double forsikringPrDag { get; set; }
+        public double prisPrKm { get; set; }
+
+        public readonly double selvrisiko = 1000;
     }
 }
