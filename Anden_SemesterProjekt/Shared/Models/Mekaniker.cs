9c98ce22ed646262deeaa785b7c53c389ddd40f6
04mag <mathiasgreisen@gmail.com>
2024-11-14 19:23:30 +0100

    Tilføjede Data Annotation til Mekaniker klasse

--- Anden_SemesterProjekt/Shared/Models/Mekaniker.cs
+++ Anden_SemesterProjekt/Shared/Models/Mekaniker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,12 @@ namespace Anden_SemesterProjekt.Shared.Models
 {
     public class Mekaniker
     {
-
         public int MekanikerId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Navn skal være mellem 2 og 50 karaktere langt!")]
         public string Navn { get; set; }
-        public bool ErAktiv { get; set; }
-        public List<Mærke> Mærker { get; set; }
-        public List<Ordre> Ordrer { get; set; }
-
+        public bool ErAktiv { get; set; } = true;
+        public List<Mærke>? Mærker { get; set; }
+        public List<Ordre>? Ordrer { get; set; }
     }
 }
