985cf0fd09cddb16bf5aa712735ae99c6e46fc90
04mag <mathiasgreisen@gmail.com>
2024-11-14 19:54:43 +0100

    Added AnsatController

---
+++ Anden_SemesterProjekt/Server/Controllers/AnsatController.cs
@@ -0,0 +1,31 @@
+﻿using Anden_SemesterProjekt.Server.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anden_SemesterProjekt.Server.Controllers
+{
+    [Route("api/ansat")]
+    [ApiController]
+    public class AnsatController : ControllerBase
+    {
+        private readonly IAnsatService _ansatService;
+
+        public AnsatController(IAnsatService ansatService)
+        {
+            _ansatService = ansatService;
+        }
+
+        [HttpGet]
+        public List<Mekaniker>? Get()
+        {
+            return _ansatService.ReadMekanikere();
+        }
+
+        [HttpGet("{id}")]
+        public Mekaniker? Get(int id)
+        {
+            return _ansatService.ReadMekaniker(id);
+        }
+    }
+}
