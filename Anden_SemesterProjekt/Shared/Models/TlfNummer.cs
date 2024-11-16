6b3f2b04cd2d699fea0eab6f16261b6ae2aef2a5
04mag <mathiasgreisen@gmail.com>
2024-11-16 13:48:46 +0100

    Updated TlfNummer model

--- Anden_SemesterProjekt/Shared/Models/TlfNummer.cs
+++ Anden_SemesterProjekt/Shared/Models/TlfNummer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Anden_SemesterProjekt.Shared.Models
     public class TlfNummer
     {
         public int TlfNummerId { get; set; }
+        [Required]
+        [RegularExpression(@"^{[2-9]\d{7}$", ErrorMessage = "Ugyldigt telefonnummer.")]
         public string TelefonNummer { get; set; }
         public int KundeId { get; set; }
         public Kunde Kunde { get; set; }
