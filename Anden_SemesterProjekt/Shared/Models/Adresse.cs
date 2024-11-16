e750a3e08129b7e77ed0aabd2a88351c5bb8d5db
04mag <mathiasgreisen@gmail.com>
2024-11-16 04:17:32 +0100

    Update to Adresse model

--- Anden_SemesterProjekt/Shared/Models/Adresse.cs
+++ Anden_SemesterProjekt/Shared/Models/Adresse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace Anden_SemesterProjekt.Shared.Models
     public class Adresse
     {
         public int AdresseId { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Gadenavn skal være mellem 2 og 100 karaktere langt!")]
         public string Gadenavn { get; set; }
+        [Required]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "Husnummer skal være mellem 2 og 5 karaktere langt!")]
         public string Husnummer { get; set; }
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "Etage skal være mellem 1 og 3 karaktere langt!")]
         public string? Etage { get; set; }
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Side skal være 1 karakter langt!")]
         public string? Side { get; set; }
+        [StringLength(5, MinimumLength = 1, ErrorMessage = "Dørnummer skal være mellem 1 og 5 karaktere langt!")]
         public string? Dørnummer { get; set; }
         public string Postnummer { get; set; }
         public By By { get; set; }
