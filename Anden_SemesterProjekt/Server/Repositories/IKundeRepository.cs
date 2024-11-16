1d24f8ffc1832ef31492bd434c99f284b5d57916
04mag <mathiasgreisen@gmail.com>
2024-11-16 03:43:18 +0100

    Added KundeRepository

--- Anden_SemesterProjekt/Server/Repositories/IKundeRepository.cs
+++ Anden_SemesterProjekt/Server/Repositories/IKundeRepository.cs
@@ -7,7 +7,7 @@ namespace Anden_SemesterProjekt.Server.Repositories
         public int CreateKunde(Kunde kunde);
         public Kunde? ReadKunde(int id);
         public List<Kunde>? ReadKunder();
-        public List<Kunde>? ReadKunder(string tlfNummer, Mærke mærke);
+        public List<Kunde>? ReadKunder(string tlfNummer, string mærke);
         public bool UpdateKunde(Kunde kunde);
         public bool DeleteKunde(int id);
         public By? GetBy(int postnummer);
--- Anden_SemesterProjekt/Server/Repositories/KundeRepository.cs
+++ Anden_SemesterProjekt/Server/Repositories/KundeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Anden_SemesterProjekt.Server.Context;
 using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Anden_SemesterProjekt.Server.Repositories
 {
@@ -10,11 +11,34 @@ namespace Anden_SemesterProjekt.Server.Repositories
         {
             _context = new SLContext();
         }
+
+        /// <summary>
+        /// Opretter ny kunde i databasen.
+        /// </summary>
+        /// <param name="kunde"></param>
+        /// <returns>Retunerer kundeId hvis kunden er oprettet, ellers -1.</returns>
         public int CreateKunde(Kunde kunde)
         {
-            throw new NotImplementedException();
+            _context.Kunder.Add(kunde);
+            _context.SaveChanges();
+
+            int id = kunde.KundeId;
+
+            if (id > 0)
+            {
+                return id;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
+        /// <summary>
+        /// Sletter kunde fra databasen.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Om sletning var successfuld som bool.</returns>
         public bool DeleteKunde(int id)
         {
             Kunde? kunde = ReadKunde(id);
@@ -31,29 +55,93 @@ namespace Anden_SemesterProjekt.Server.Repositories
             }
         }
 
+        /// <summary>
+        /// Finder by ud fra postnummer.
+        /// </summary>
+        /// <param name="postnummer"></param>
+        /// <returns>Retunerer By Object fra postnummer. Null hvis ikke fundet.</returns>
         public By? GetBy(int postnummer)
         {
             return _context.Byer.Find(postnummer);
         }
 
+        /// <summary>
+        /// Finder kunde ud fra id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Retunerer kunde object. Null hvis ikke fundet.</returns>
         public Kunde? ReadKunde(int id)
         {
             return _context.Kunder.Find(id);
         }
 
+        /// <summary>
+        /// Finder alle kunder i databasen.
+        /// </summary>
+        /// <returns>Retunerer liste af kunder. Null hvis ingen fundet.</returns>
         public List<Kunde>? ReadKunder()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _context.Kunder
+                    .Include(k => k.TlfNumre)
+                    .Include(k => k.Adresse).ThenInclude(a => a.By)
+                    .Include(k => k.TilknyttetMekaniker)
+                    .Include(k => k.Scootere).ThenInclude(m => m.Mærke)
+                    .Include(k => k.Ordrer)
+                    .ToList();
+            }
+            catch
+            {
+                return null;
+            }
         }
 
-        public List<Kunde>? ReadKunder(string tlfNummer, Mærke mærke)
+        /// <summary>
+        /// Finder kunder ud fra tlfNummer og mærke søgning.
+        /// </summary>
+        /// <param name="tlfNummer"></param>
+        /// <param name="mærke"></param>
+        /// <returns>Retunerer liste af kunder. Null hvis ikke fundet</returns>
+        public List<Kunde>? ReadKunder(string tlfNummer, string mærke)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _context.Kunder
+                    .Include(k => k.TlfNumre)
+                    .Include(k => k.Adresse).ThenInclude(a => a.By)
+                    .Include(k => k.TilknyttetMekaniker)
+                    .Include(k => k.Scootere).ThenInclude(m => m.Mærke)
+                    .Include(k => k.Ordrer)
+                    .Where(k => k.TlfNumre.Any(t => t.TelefonNummer.Contains(tlfNummer, StringComparison.OrdinalIgnoreCase)))
+                    .Where(k => k.Scootere.Any(s => s.Mærke.ScooterMærke.Contains(mærke, StringComparison.OrdinalIgnoreCase)))
+                    .ToList();
+            }
+            catch
+            {
+                return null;
+            }
         }
 
+        /// <summary>
+        /// Opdaterer kunde i databasen.
+        /// </summary>
+        /// <param name="kunde"></param>
+        /// <returns>Om opdatering var successfuld som bool.</returns>
         public bool UpdateKunde(Kunde kunde)
         {
-            throw new NotImplementedException();
+            var result = _context.Kunder.Find(kunde.KundeId);
+
+            if (result == null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Update(kunde);
+                _context.SaveChanges();
+                return true;
+            }
         }
     }
 }
