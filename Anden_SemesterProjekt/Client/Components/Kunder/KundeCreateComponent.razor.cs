3914a3512e737579b52eb220b3942c6c897adb69
04mag <mathiasgreisen@gmail.com>
2024-11-16 14:46:56 +0100

    Added more code to KundeCreateComponent

--- Anden_SemesterProjekt/Client/Components/Kunder/KundeCreateComponent.razor.cs
+++ Anden_SemesterProjekt/Client/Components/Kunder/KundeCreateComponent.razor.cs
@@ -1,4 +1,6 @@
-﻿using Anden_SemesterProjekt.Shared.Models;
+﻿using Anden_SemesterProjekt.Client.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace Anden_SemesterProjekt.Client.Components.Kunder
@@ -7,20 +9,59 @@ namespace Anden_SemesterProjekt.Client.Components.Kunder
     {
         private Kunde kundeModel = new Kunde();
         private EditContext editContext;
+        private bool isSubmitting = false;
+        private string postnummerErrorMessage = "";
+        private string byNavn = "";
+
+        [Inject]
+        public IKundeClientService KundeService { get; set; }
 
         protected override void OnInitialized()
         {
             editContext = new EditContext(kundeModel);
         }
 
-        private void HandleValidSubmit()
+        private async void HandleValidSubmit()
         {
-            Console.WriteLine("Form submitted");
+            isSubmitting = true;
+
+            By by = await KundeService.GetBy(kundeModel.Adresse.Postnummer);
+
+            if (by == null)
+            {
+                postnummerErrorMessage = "Postnummeret findes ikke";
+            }
+            else
+            {
+
+            }
+            isSubmitting = false;
         }
 
         private void HandleInvalidSubmit()
         {
             Console.WriteLine("Form not submitted");
+        }
+
+        private async Task<string> GetBynavn()
+        {
+            var result = await KundeService.GetBy(kundeModel.Adresse.Postnummer);
+
+            if (result == null)
+            {
+                postnummerErrorMessage = "Postnummeret findes ikke";
+                return "";
+            }
+            else
+            {
+                postnummerErrorMessage = "";
+                return result.ByNavn;
+            }
+        }
+
+        private async void PostnummerChanged()
+        {
+            byNavn = await GetBynavn();
         }
     }
 }
