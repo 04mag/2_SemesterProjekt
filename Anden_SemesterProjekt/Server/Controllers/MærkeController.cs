89144d7fc13a173228d892eaefcbac3d37dacf54
Sorenbb97 <sorenbb97@gmail.com>
2024-11-16 20:39:50 +0100

    uudlejnigsscooter controller oprettet

---
+++ Anden_SemesterProjekt/Server/Controllers/MærkeController.cs
@@ -0,0 +1,48 @@
+﻿using Anden_SemesterProjekt.Server.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anden_SemesterProjekt.Server.Controllers
+{
+    [Route("api/ansat")]
+    [ApiController]
+    public class MærkeController : ControllerBase
+    {
+        private readonly IMærkeService _mærkeService;
+
+        public MærkeController(IMærkeService mærkeService)
+        {
+            _mærkeService = mærkeService;
+        }
+
+        [HttpGet]
+        public List<Mærke>? Get()
+        {
+            return _mærkeService.ReadMærke();
+        }
+
+        [HttpGet("{id}")]
+        public Mærke? Get(int id)
+        {
+            return _mærkeService.ReadMærke(id);
+        }
+
+        [HttpPost]
+        public int Post(Mærke mærke)
+        {
+            return _mærkeService.CreateMærke(mærke);
+        }
+        [HttpPut]
+        public int Put(Mærke mærke)
+        {
+            return _mærkeService.UpdateMærke(mærke);
+        }
+        [HttpDelete]
+        public int Delete(int id)
+        {
+            return _mærkeService.DeleteMærke(id);
+        }
+
+    }
+}
---
+++ Anden_SemesterProjekt/Server/Controllers/UdlejningsScooterController.cs
@@ -0,0 +1,49 @@
+﻿using Anden_SemesterProjekt.Server.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anden_SemesterProjekt.Server.Controllers
+{
+    [Route("api/ansat")]
+    [ApiController]
+    public class UdlejningsScooterController : ControllerBase
+    {
+        private readonly IUdlejningsScooterService _udlejningsScooterService;
+
+        public UdlejningsScooterController(IUdlejningsScooterService udlejningsScooterService)
+        {
+            _udlejningsScooterService = udlejningsScooterService;
+        }
+
+        [HttpGet]
+        public List<UdlejningsScooter>? Get()
+        {
+            return _udlejningsScooterService.ReadUdlejningsScootere();
+        }
+
+        [HttpGet("{id}")]
+        public UdlejningsScooter? Get(int id)
+        {
+            return _udlejningsScooterService.ReadUdlejningsScooter(id);
+        }
+
+        [HttpPost]
+        public int Post(UdlejningsScooter udlejningsScooter)
+        {
+            return _udlejningsScooterService.CreateUdlejningsScooter(udlejningsScooter);
+        }
+
+        [HttpPut]
+        public int Put(UdlejningsScooter udlejningsScooter)
+        {
+            return _udlejningsScooterService.UpdateUdlejningsScooter(udlejningsScooter);
+        }
+
+        [HttpDelete]
+        public int Delete(int id)
+        {
+            return _udlejningsScooterService.DeleteUdlejningsScooter(id);
+        }
+    }
+}
