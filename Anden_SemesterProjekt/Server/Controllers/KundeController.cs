08992fe29e58da4608b88ab58e342cbfff27993d
04mag <mathiasgreisen@gmail.com>
2024-11-16 04:02:54 +0100

    Added KundeController

--- Anden_SemesterProjekt/Server/Controllers/KundeController.cs
+++ Anden_SemesterProjekt/Server/Controllers/KundeController.cs
@@ -21,10 +21,34 @@ namespace Anden_SemesterProjekt.Server.Controllers
             return _kundeService.ReadKunder();
         }
 
+        [HttpGet("{tlfnummer}/{mærke}")]
+        public List<Kunde>? GetKunder(string tlfnummer, string mærke)
+        {
+            return _kundeService.ReadKunder(tlfnummer, mærke);
+        }
+
+        [HttpGet("{id}")]
+        public Kunde? GetKunde(int id)
+        {
+            return _kundeService.ReadKunde(id);
+        }
+
         [HttpPost]
         public int PostKunde(Kunde kunde)
         {
             return _kundeService.CreateKunde(kunde);
+        }
+
+        [HttpPut]
+        public bool PutKunde(Kunde kunde)
+        {
+            return _kundeService.UpdateKunde(kunde);
+        }
+
+        [HttpDelete("{id}")]
+        public bool DeleteKunde(int id)
+        {
+            return _kundeService.DeleteKunde(id);
         }
     }
 }
