d7b3866fdf4a9e14b1202717c36d8fffdcce5e43
04mag <mathiasgreisen@gmail.com>
2024-11-16 04:14:13 +0100

    Made changes to Kunde Model

--- Anden_SemesterProjekt/Shared/Models/Kunde.cs
+++ Anden_SemesterProjekt/Shared/Models/Kunde.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,16 @@ namespace Anden_SemesterProjekt.Shared.Models
     {
 
         public int KundeId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Navn skal være mellem 2 og 50 karaktere langt.")]
         public string Navn { get; set; }
         public Adresse Adresse { get; set; }
         public List<TlfNummer> TlfNumre { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Ugyldig Email.")]
         public string Email { get; set; }
-        public Mekaniker TilknyttetMekaniker { get; set; }
-        public List<KundeScooter> Scootere { get; set; }
-        public List<Ordre> Ordrer { get; set; }
+        public Mekaniker? TilknyttetMekaniker { get; set; }
+        public List<KundeScooter>? Scootere { get; set; }
+        public List<Ordre>? Ordrer { get; set; }
     }
 }
