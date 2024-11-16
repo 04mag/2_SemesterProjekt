ea0ce6a173d4ce5179c79fa13a1978f7be622f0d
04mag <mathiasgreisen@gmail.com>
2024-11-16 03:57:06 +0100

    Updated IKundeRepository and IKundeService

---
+++ Anden_SemesterProjekt/Server/Controllers/KundeController.cs
@@ -0,0 +1,30 @@
+﻿using Anden_SemesterProjekt.Server.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anden_SemesterProjekt.Server.Controllers
+{
+    [Route("api/kunder")]
+    [ApiController]
+    public class KundeController : ControllerBase
+    {
+        private readonly IKundeService _kundeService;
+        public KundeController(IKundeService kundeService)
+        {
+            _kundeService = kundeService;
+        }
+
+        [HttpGet]
+        public List<Kunde>? GetKunder()
+        {
+            return _kundeService.ReadKunder();
+        }
+
+        [HttpPost]
+        public int PostKunde(Kunde kunde)
+        {
+            return _kundeService.CreateKunde(kunde);
+        }
+    }
+}
--- Anden_SemesterProjekt/Server/Services/IKundeService.cs
+++ Anden_SemesterProjekt/Server/Services/IKundeService.cs
@@ -4,12 +4,53 @@ namespace Anden_SemesterProjekt.Server.Services
 {
     public interface IKundeService
     {
+        /// <summary>
+        /// Opretter ny kunde i databasen.
+        /// </summary>
+        /// <param name="kunde"></param>
+        /// <returns>Retunerer kundeId hvis kunden er oprettet, ellers -1.</returns>
         public int CreateKunde(Kunde kunde);
+
+        /// <summary>
+        /// Finder kunde ud fra id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Retunerer kunde object. Null hvis ikke fundet.</returns>
         public Kunde? ReadKunde(int id);
+
+        /// <summary>
+        /// Finder alle kunder i databasen.
+        /// </summary>
+        /// <returns>Retunerer liste af kunder. Null hvis ingen fundet.</returns>
         public List<Kunde>? ReadKunder();
+
+        /// <summary>
+        /// Finder kunder ud fra tlfNummer og mærke søgning.
+        /// </summary>
+        /// <param name="tlfNummer"></param>
+        /// <param name="mærke"></param>
+        /// <returns>Retunerer liste af kunder. Null hvis ikke fundet</returns>
         public List<Kunde>? ReadKunder(string tlfNummer, string mærke);
+
+        /// <summary>
+        /// Opdaterer kunde i databasen.
+        /// </summary>
+        /// <param name="kunde"></param>
+        /// <returns>Om opdatering var successfuld som bool.</returns>
         public bool UpdateKunde(Kunde kunde);
+
+        /// <summary>
+        /// Sletter kunde fra databasen.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Om sletning var successfuld som bool.</returns>
         public bool DeleteKunde(int id);
+
+        /// <summary>
+        /// Finder by ud fra postnummer.
+        /// </summary>
+        /// <param name="postnummer"></param>
+        /// <returns>Retunerer By Object fra postnummer. Null hvis ikke fundet.</returns>
         public By? GetBy(int postnummer);
     }
 }
