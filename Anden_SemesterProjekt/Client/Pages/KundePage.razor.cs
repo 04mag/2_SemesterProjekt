c3348ccf08b7386702eb9f98cbe1d228c570fc2f
04mag <mathiasgreisen@gmail.com>
2024-11-18 14:11:53 +0100

    Was missing Inject in KundePage

--- Anden_SemesterProjekt/Client/Pages/KundePage.razor.cs
+++ Anden_SemesterProjekt/Client/Pages/KundePage.razor.cs
@@ -1,32 +1,25 @@
 ﻿
 using Anden_SemesterProjekt.Client.Services;
 using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace Anden_SemesterProjekt.Client.Pages
 {
     public partial class KundePage
     {
         private List<Kunde>? kunder;
+
+        [Inject]
         public IKundeClientService KundeService { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Console.WriteLine("Før indlæsning");
-            var result = await KundeService.GetKunder();
-            Console.WriteLine($"Midt i indlæsning: {result.GetType()}");
-            kunder = result;
+            kunder = await KundeService.GetKunder();
         }
 
         private async Task OnKundeAddedHandler()
         {
-            try
-            {
-                kunder = await KundeService.GetKunder();
-            }
-            catch
-            {
-                kunder = null;
-            }
+            kunder = await KundeService.GetKunder();
         }
 
         private async Task OnEditKunde(Kunde kunde)
