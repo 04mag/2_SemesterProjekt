ace57f4c65cd85363303c3880e0ecb7c057f762f
Oliver <165995824+oliverbeckert@users.noreply.github.com>
2024-11-20 14:12:14 +0100

    Tilføjede IScooterService

---
+++ Anden_SemesterProjekt/Server/Services/IScooterService.cs
@@ -0,0 +1,15 @@
+﻿using Anden_SemesterProjekt.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Anden_SemesterProjekt.Server.Services
+{
+    public interface IScooterService
+    {
+        Task<List<Scooter>> GetAllScootersAsync();
+        Task<Scooter?> GetScooterByIdAsync(int id);
+        Task<int> AddScooterAsync(Scooter scooter);
+        Task<bool> UpdateScooterAsync(Scooter scooter);
+        Task<bool> DeleteScooterAsync(int id);
+    }
+}
