bd1618243bab14de01b1d10b5302ca26e22d849b
04mag <mathiasgreisen@gmail.com>
2024-11-20 18:19:30 +0100

    Added migration for ErAktiv i kunde

---
+++ Anden_SemesterProjekt/Server/Migrations/20241120171907_addedErAktivTilKundeModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anden_SemesterProjekt.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedErAktivTilKundeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ErAktiv",
+                table: "Kunder",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErAktiv",
+                table: "Kunder");
+        }
+    }
+}
