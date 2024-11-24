ae662a7bbcfd43e886651828b6cbe06a6e919ef7
Frederik Lyager <frederiklyager@gmail.com>
2024-11-24 02:14:59 +0100

    VarePage.razor og VarePage.razor.cs oprettet - check for errors

--- Anden_SemesterProjekt/Client/Pages/Varer/VarePage.razor.cs
+++ Anden_SemesterProjekt/Client/Pages/Varer/VarePage.razor.cs
@@ -4,15 +4,17 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.JSInterop;
 using Anden_SemesterProjekt.Shared.Models;
 
-namespace Anden_SemesterProjekt.Client.Pages.Vare
+namespace Anden_SemesterProjekt.Client.Pages.Varer
 {
     public partial class VarePage
     {
         private List<Vare>? varer;
 
+        //En service, som skal injectes til at hente og manipulere data fra serveren via api kaldet. 
         [Inject]
         public IVareClientService VareService { get; set; }
 
+        //En service, som skal injectes for at kunne bruge JavaScript interop i Blazor, feks. alert , confirm. 
         [Inject]
         private IJSRuntime JS { get; set; }
 
@@ -31,32 +33,42 @@ namespace Anden_SemesterProjekt.Client.Pages.Vare
                     varer = new List<Vare>();
                 }
             }
+            //Hvis der opstår en exception under hentning af varer, så vises en alert med fejlbeskeden.
             catch (Exception e)
             {
                 await JS.InvokeVoidAsync("alert", "Der skete en fejl under hentning af varer." + e.Message);
 
             }
         }
+
+        //Denne metode kaldes, når komponenten er loaded. For at hente listen over varer fra serveren.
         protected override async Task OnInitializedAsync()
         {
             await UpdateVare();
         }
 
+        //Denne metoder opdaterer listen over varer, når der er tilføjet en ny vare. Ved genkald af metoden "UpdateVare".
         private async Task OnVareAddedHandler()
         {
             await UpdateVare();
         }
 
+        //Denne metode navigerer brugeren til en ny side, hvor der vises detaljer om en specifik vare, baseret på varens ID.
         private void OnSelectVare(Vare vare)
         {
-            NavigationManager.NavigateTo($"/vare/{vare.Id}");
+            NavigationManager.NavigateTo($"/varer/{vare.Id}");
         }
 
+        //Denne metode navigerer brugeren til en redigere side, hvor brugeren kan redigere en specifik vare, baseret på varens ID.
         private void OnEditKunde(Vare vare)
         {
-            NavigationManager.NavigateTo($"/vare/edit/{vare.Id}");
+            NavigationManager.NavigateTo($"/varer/edit/{vare.Id}");
         }
 
+        //Viser først en JS confirm dialog, som spørger brugeren om de er sikker på at de vil slette varen.
+        //Derefter kalder metoden DeleteVare fra VareService, som sletter varen fra databasen.
+        //Hvis sletningen lykkedes, så opdateres listen over varer.
+        //Hvis sletningen fejler, så vises en alert med fejlbeskeden.
         private async Task OnDeleteVare(Vare vare) 
         {
             var confirmDelete = await JS.InvokeAsync<bool>("confirm", "Er du sikker på at du vil slette denne vare?");
