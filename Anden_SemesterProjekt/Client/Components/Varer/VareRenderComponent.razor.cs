b93a8f17cb67bb42fa03df49a98a38950010acf4
Frederik Lyager <frederiklyager@gmail.com>
2024-11-24 03:02:04 +0100

    VareRenderComponent.razor.cs oprettet. VareRenderComponent.razor stil pending. Comit'n push for save

---
+++ Anden_SemesterProjekt/Client/Components/Varer/VareRenderComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Anden_SemesterProjekt.Client.Components.Varer
+{
+    public partial class VareRenderComponent
+    {
+        [Parameter, EditorRequired]
+        public Vare? VareModel { get; set; }
+
+        [Parameter]
+        public EventCallback<Vare> OnSelectVare { get; set; }
+        [Parameter]
+        public EventCallback<Vare> OnEditVare { get; set; }
+        [Parameter]
+        public EventCallback<Vare> OnDeleteVare { get; set; }
+        
+    }
+}
