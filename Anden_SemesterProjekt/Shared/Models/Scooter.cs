5e066dd53bc72615e75a6d57d049a65838527711
Oliver <165995824+oliverbeckert@users.noreply.github.com>
2024-11-25 17:29:56 +0100

    updated scooter model

--- Anden_SemesterProjekt/Shared/Models/Scooter.cs
+++ Anden_SemesterProjekt/Shared/Models/Scooter.cs
@@ -8,16 +8,24 @@ using System.Threading.Tasks;
 
 namespace Anden_SemesterProjekt.Shared.Models
 {
-    public abstract class Scooter
-    {
-        public int ScooterId { get; set; }
-        [Required(ErrorMessage = "Stelnummer er påkrævet.")]
-        public string Stelnummer { get; set; }
-        public string? Registreringsnummer { get; set; }
-        
-        public int MærkeId { get; set; }
+    
+        public abstract class Scooter
+        {
+            public int ScooterId { get; set; }
 
-        //[Required(ErrorMessage = "Mærke er påkrævet.")]
-        [JsonIgnore] public Mærke? Mærke { get; set; }
-    }
+            [Required(ErrorMessage = "Stelnummer er påkrævet.")]
+            [StringLength(17, MinimumLength = 17, ErrorMessage = "Stelnummer skal være præcis 17 tegn.")] //Alle stelnumre efter 1981 har 17 karaktere
+            public string Stelnummer { get; set; } = string.Empty;
+
+            [Required(ErrorMessage = "Registreringsnummer er påkrævet.")]
+            [RegularExpression(@"^[A-Za-z0-9]{1,10}$", ErrorMessage = "Registreringsnummer skal være mellem 1 og 10 alfanumeriske tegn.")]
+            public string Registreringsnummer { get; set; } = string.Empty;
+
+            public int MærkeId { get; set; }
+
+            [JsonIgnore]
+            public Mærke? Mærke { get; set; }
+        }
+
+
 }
