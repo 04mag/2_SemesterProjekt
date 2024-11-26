29ba4bcf635d97b4265e2d4f07045aba6bbe7028
04mag <mathiasgreisen@gmail.com>
2024-11-26 10:36:26 +0100

    Added new migration

---
+++ Anden_SemesterProjekt/Server/Migrations/20241126093608_tptVarerYdelser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anden_SemesterProjekt.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class tptVarerYdelser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ydelser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    AntalTimer = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ydelser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ydelser_Varer_Id",
+                        column: x => x.Id,
+                        principalTable: "Varer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ydelser");
+        }
+    }
+}
