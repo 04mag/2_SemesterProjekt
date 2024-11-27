72bc5bb1210c8d503ce854f8002dfff70698f041
04mag <mathiasgreisen@gmail.com>
2024-11-27 10:54:28 +0100

    Added OrdreEditPage

---
+++ Anden_SemesterProjekt/Client/Pages/Ordrer/OrdreEditPage.razor.cs
@@ -0,0 +1,34 @@
+﻿using Anden_SemesterProjekt.Client.Services;
+using Anden_SemesterProjekt.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Anden_SemesterProjekt.Client.Pages.Ordrer
+{
+    public partial class OrdreEditPage
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        public IOrdreClientService OrdreService { get; set; }
+
+        public Ordre? OrdreModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Console.WriteLine("Ordre indhentes!");
+            Ordre? ordreResult = await OrdreService.GetOrdre(Id);
+            
+            if (ordreResult == null)
+            {
+                Console.WriteLine("Ordre er lig null!");
+            }
+
+            if (ordreResult != null)
+            {
+                Console.WriteLine("Ordre er ikke null!");
+                OrdreModel = ordreResult;
+            }
+        }
+    }
+}
