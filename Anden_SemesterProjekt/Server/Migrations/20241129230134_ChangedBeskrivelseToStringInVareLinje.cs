0d1e9fcfee933559eae5875f3fd0f136a05a704c
04mag <mathiasgreisen@gmail.com>
2024-11-30 00:03:47 +0100

    Added new migration

---
+++ Anden_SemesterProjekt/Server/Migrations/20241129230134_ChangedBeskrivelseToStringInVareLinje.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anden_SemesterProjekt.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedBeskrivelseToStringInVareLinje : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VareBeskrivelse",
+                table: "VareLinjer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VareBeskrivelse",
+                table: "VareLinjer",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
