892d78b2bce43e795633f6ee40f660e84184e90b
04mag <mathiasgreisen@gmail.com>
2024-12-09 14:20:58 +0100

    Added print button to OrdreInfoPage

---
+++ Anden_SemesterProjekt/Client/Pages/Ordrer/OrdreInfoPage.razor.js
@@ -0,0 +1,10 @@
+﻿export function Print() {
+    for (let el of document.querySelectorAll('.hideWhilePrint')) el.style.display = 'none';
+    window.print();
+    for (let el of document.querySelectorAll('.hideWhilePrint')) el.style.display = 'block';
+}
+
+export function addHandlers() {
+    const btn = document.getElementById("PrintBtn");
+    btn.addEventListener("click", Print);
+}
