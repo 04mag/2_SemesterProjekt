0c3880b23751b49e426e636b73bf711f4383c85d
04mag <mathiasgreisen@gmail.com>
2024-12-12 16:28:15 +0100

    Added final migration

---
+++ Anden_SemesterProjekt/Server/Migrations/20241212152655_FinalMigration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anden_SemesterProjekt.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Ordrer",
+                keyColumn: "OrdreId",
+                keyValue: 2,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 12, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Ordrer",
+                keyColumn: "OrdreId",
+                keyValue: 3,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 12, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 12, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Udlejninger",
+                keyColumn: "UdlejningId",
+                keyValue: 2,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 12, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Ordrer",
+                keyColumn: "OrdreId",
+                keyValue: 2,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 10, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Ordrer",
+                keyColumn: "OrdreId",
+                keyValue: 3,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Udlejninger",
+                keyColumn: "UdlejningId",
+                keyValue: 2,
+                columns: new[] { "SlutDato", "StartDato" },
+                values: new object[] { new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2024, 12, 10, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
